c6e216b95ebcd871efddd840dfd270ca810754de
Ba Thinh Tran <s8batran@stud.uni-saarland.de>
2015-06-13 21:55:10 +0200

    7

--- src/ttfe/tests/SimpleTests.java
+++ src/ttfe/tests/SimpleTests.java
@@ -94,6 +94,7 @@ public class SimpleTests {
 		game.setPieceAt(2, 1, 2);
 		game.setPieceAt(2, 2, 2);
 		game.setPieceAt(2, 3, 2);
+		game.setPieceAt(2, 0, 0);
 		int y = game.getNumPieces();
 		game.addPiece();
 		assertTrue("getNumPieces has a malfunction"+y + game.getNumPieces() ,y == game.getNumPieces()); 
