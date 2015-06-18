52493e3a80cd0faf165229759529b9a6437a9042
Ba Thinh Tran <s8batran@stud.uni-saarland.de>
2015-06-18 20:46:56 +0200

    haha

---
+++ src/ttfe/AI.java
@@ -0,0 +1,11 @@
+package ttfe;
+
+public class AI implements PlayerInterface {
+
+	@Override
+	public MoveDirection getPlayerMove(SimulatorInterface game, UserInterface ui) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/ttfe/Cat.java
@@ -0,0 +1,31 @@
+package ttfe;
+
+import ttfe.PlayerInterface;
+
+public class Cat implements PlayerInterface {
+	
+	String direction ; 
+
+	@Override
+	public MoveDirection getPlayerMove(SimulatorInterface game, UserInterface ui) {
+		// TODO Auto-generated method stub
+		
+		String question = "Direction ?" ;
+		String[] possibleAnswers = new String[4]; 
+		
+		possibleAnswers[0] = "r";
+		possibleAnswers[1] = "l";
+		possibleAnswers[2] = "u";
+		possibleAnswers[3] = "d";
+		
+		direction = ui.getUserInput(question, possibleAnswers) ;
+		
+		if (direction.charAt(0) == 'r' ) return MoveDirection.EAST ;
+		else if (direction.charAt(0) == 'l') return MoveDirection.WEST ;
+		else if (direction.charAt(0) == 'u') return MoveDirection.NORTH ;
+		else if (direction.charAt(0) == 'd') return MoveDirection.SOUTH ;
+		
+		return null;
+	}
+
+}
